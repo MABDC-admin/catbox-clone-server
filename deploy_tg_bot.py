0e301fdfd00fee1de1ca65570cb060a631ce27b0
MABDC-admin <admin@mabdc.ae>
2026-07-11 15:32:14 +0400

    Disable concurrent fragment downloading entirely for non-YouTube sites to prevent Dailymotion race conditions

--- deploy_tg_bot.py
+++ deploy_tg_bot.py
@@ -83,13 +83,12 @@ import os
 import time
 
 UPLOAD_DIR = "/home/admin/catbox-clone-server/uploads"
-SIX_DAYS = 6 * 24 * 60 * 60
+cutoff_time = time.time() - (1 * 24 * 60 * 60) # 1 day
 
-now = time.time()
 for f in os.listdir(UPLOAD_DIR):
     path = os.path.join(UPLOAD_DIR, f)
     if os.path.isfile(path):
-        if os.stat(path).st_mtime < now - SIX_DAYS:
+        if os.stat(path).st_mtime < cutoff_time:
             try:
                 os.remove(path)
             except:
